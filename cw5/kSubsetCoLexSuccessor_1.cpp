d32c319905b11f9d2a8d012ff9714cf4afee18d9
Natalia Postawa <naciap@users.noreply.github.com>
2016-04-06 12:56:34 +0200

    Update kSubsetCoLexSuccessor_1.cpp

--- cw5/kSubsetCoLexSuccessor_1.cpp
+++ cw5/kSubsetCoLexSuccessor_1.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 using namespace std;
 
+// Napisz program wyznaczający następnik k-elementowego podzbioru T zbioru {1, . . . , n}
+// w uporządkowaniu antyleksykograficznym podzbiorów k-elementowych.
+
 int main() {
 int n, k;
 
@@ -14,19 +17,24 @@ for(int i=1; i<=k; i++){
 }
 
 T[0]=n+1;
+
 int i=k;
+
 while (i>0 && T[i]==T[i-1]-1){
 	i--;
 }
+
 if(i==0) {
 	printf("Brak nastepnika");
 	return 0;}
+
 T[i]=T[i]+1;
+
 for(int j=i+1; i<=k; i++){
 	T[j]=k-j+1;
 }
 
-for(i=1;i<=k;i++){
+for(i=1; i<=k; i++){
 	printf("%d ", T[i]);
 }
 	
